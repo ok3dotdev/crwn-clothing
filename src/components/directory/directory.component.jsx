f32ac6454159321985e02bfae33476d13de100e9
Henry <new@Henrys-macbook-pro.local>
2020-08-03 04:03:23 -0400

    components updated

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -39,7 +39,7 @@ class Directory extends React.Component {
   }
   render() {
     return (
-      <div className="directory menu">
+      <div className="directory-menu">
         {this.state.sections.map(({ title, imageUrl, id }) => (
           <MenuItem key={id} title={title} />
         ))}
