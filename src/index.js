7efdedb4676a083f584e5911e7ffc025bcc63614
Henry <new@Henrys-macbook-pro.local>
2021-01-01 19:26:37 -0500

    IMPLEMENT REDUX WITH USERACTION TO HEADER

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+
+import store from './redux/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
